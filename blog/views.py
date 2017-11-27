e9434da0c0d5e6b3261a7aaa639a217bf0ad74fe
wild law <anhojun7949@gmail.com>
2017-11-27 12:12:15 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+#models에 있는 파일을 가져와 준다.
+from django.utils import timezone
+from .models import Post
+
+
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+	#published_date 기준으로 정리해준다.
+	posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+                                                  #템플릿을 추가하기 위해 매개변수를 추가
+
+
